c06a7077b0bc488b24b3d9b54ab77bddd6166d93
Stu Heiss <sheiss@highlandsolutions.com>
2015-12-09 23:47:51 +0000

    FEAT(AlbumController.php) add meth getAlbumTable()

--- module/Album/src/Album/Controller/AlbumController.php
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -6,6 +6,8 @@ use Zend\View\Model\ViewModel;
 
 class AlbumController extends AbstractActionController
 {
+    protected $albumTable;
+
     public function indexAction()
     {
     }
@@ -20,5 +22,14 @@ class AlbumController extends AbstractActionController
 
     public function deleteAction()
     {
+    }
+
+    public function getAlbumTable()
+    {
+        if (!$this->albumTable) {
+            $sm = $this->getServiceLocator();
+            $this->albumTable = $sm->get('Album\Model\AlbumTable');
+        }
+        return $this->albumTable;
     }
 }
