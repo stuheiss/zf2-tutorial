3af875f20536c5720efc48557ad733923fc06d63
Stu Heiss <sheiss@highlandsolutions.com>
2015-12-09 23:01:57 +0000

    NEW module/Album/config/module.config.php

---
+++ module/Album/config/module.config.php
@@ -0,0 +1,12 @@
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Album\Controller\Album' => 'Album\Controller\AlbumController',
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'album' => __DIR__ . '/../view',
+        ),
+    ),
+);
